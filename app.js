5a2b3f250739f99702e33d41cb5456c60eb7105c
JimmyHuang3364 <open3364@hotmail.com>
2021-10-05 23:35:48 +0800

    添加路由

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
 
+const routes = require('./routes')
+
 require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.static('public'))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
---
+++ routes/modules/home.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+// const URLshortener = require('../../models/URLshortener')
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+module.exports = router

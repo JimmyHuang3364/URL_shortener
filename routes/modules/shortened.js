cf34dabd291af976c981e4824b59d1a3beb3c230
JimmyHuang3364 <open3364@hotmail.com>
2021-10-08 01:23:47 +0800

    新增invalidURL.hbs

--- routes/modules/shortened.js
+++ routes/modules/shortened.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  return res.render('shortened')
+// router.get('/', (req, res) => {
+//   return res.render('shortened')
+// })
+
+router.post('/', (req, res) => {
+  res.render('shortened')
 })
 
 module.exports = router

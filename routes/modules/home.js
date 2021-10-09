198a51b214940db33216f35edc5e5870a6e89488
JimmyHuang3364 <open3364@hotmail.com>
2021-10-09 21:36:20 +0800

    完成簡址跳轉路由

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const router = express.Router()
-
-// const URLshortener = require('../../models/URLshortener')
+const URLshortener = require('../../models/URLshortener')
 
 router.get('/', (req, res) => {
   res.render('index')
 })
+
+router.get('/:shortURLdata', (req, res) => {
+  const shortURLdata = req.params.shortURLdata
+  URLshortener.findOne({ shortURL: shortURLdata })
+    .then((URLdata) => {
+      console.log('asdasd')
+      console.log(URLdata.originalURL)
+      res.redirect(`${URLdata.originalURL}`)
+    })
+})
+
+
 
 module.exports = router
